56f1cd62e7d1674c35b78b7f7be4369534884ea3
sho <sho.obata.c5@gmail.com>
2024-01-07 15:22:06 +0900

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, abort, url_for, render_template
 import requests
 import json
 # import marshmallow as ma
-from flask_smorest import Api, Blueprint, abort
+# from flask_smorest import Api, Blueprint, abort
 from mysql_files import gen_mission
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ app.config["API_TITLE"] = "My API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.2"
 app.json.ensure_ascii = False
-api = Api(app)
+# api = Api(app)
 
 # class PetSchema(ma.Schema):
 #     id = ma.fields.Int(dump_only=True)
